481a46bd0e241f79ab437b05a8621212ec92ed8d
Dan Fox <dan.fox@laterooms.com>
2015-12-24 11:12:38 +0000

    added logging

--- src/message-relay.js
+++ src/message-relay.js
@@ -1,21 +1,30 @@
 var AWS = require('aws-sdk');
 var Consumer = require('sqs-consumer');
 
+var robot;
+
 function handleMessage(message, done) {
+    robot.logger.debug('handleMessage: ' + JSON.stringify(message));
     if (message.message) {
+        robot.logger.debug('message is: ' + message.message);
         var envelope = {
             room: message.channel || '#here-be-raptors'
         };
+        robot.logger.debug('envelope is: ' + envelope);
 
         var messages = [message.message];
 
         robot.send(envelope, messages);
     }
 
+    robot.logger.debug('calling done');
+
     done();
 }
 
-module.exports = function (robot) {
+module.exports = function (robotParam) {
+
+    robot = robotParam;
 
     var key = process.env.HUBOT_SQS_MESSAGE_RELAY_ACCESS_KEY_ID;
     var secret = process.env.HUBOT_SQS_MESSAGE_RELAY_SECRET_ACCESS_KEY;
