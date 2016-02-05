3642b5a14628b18548b5f667fa167662954e83c7
Dan Fox <dan.fox@laterooms.com>
2016-02-05 15:50:12 +0000

    added say

---
+++ src/say.js
@@ -0,0 +1,16 @@
+module.exports = function (robot) {
+    robot.router.post('/hubot/say/:room', function (req, res) {
+            room = req.params.room
+
+            if (req.body.payload) {
+                data = JSON.parse(req.body.payload).message;
+            } else {
+                data = req.body;
+            }
+
+            robot.messageRoom(room, data);
+
+            res.send('OK');
+        };
+    };
+}
