5bb0bc81d7abd68fdeb3e38852ede0cb03683aac
Jasmeet Khalsa <jasmeet.khalsa@laterooms.com>
2016-06-21 11:22:29 +0100

    Added startup message

---
+++ src/startup.js
@@ -0,0 +1,3 @@
+module.exports = function(robot) {
+    robot.messageRoom("#here-be-raptors", "God creates dinosaurs. God destroys dinosaurs. God creates man. Man destroys God. Man creates dinosaurs.");
+}
