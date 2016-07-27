d9294600e68c79c88c073c4d5c8d74d45add6a38
Dan Fox <dan.fox@laterooms.com>
2016-07-27 08:27:42 +0100

    sanitize room names

--- src/startup.js
+++ src/startup.js
@@ -2,7 +2,10 @@ var roomSanitizer = require('../lib/roomSanitizer');
 
 module.exports = function(robot) {
     if (process.env.HUBOT_STARTUP_MESSAGE_ROOM && process.env.HUBOT_STARTUP_MESSAGE) {
-        robot.messageRoom(roomSanitizer(process.env.HUBOT_STARTUP_MESSAGE_ROOM), process.env.HUBOT_STARTUP_MESSAGE);
+        var room = roomSanitizer(process.env.HUBOT_STARTUP_MESSAGE_ROOM);
+        var message = process.env.HUBOT_STARTUP_MESSAGE;
+        robot.logger.info("Sending: " + message + " to: " + room);
+        robot.messageRoom(room, message);
     } else {
         console.log("HUBOT_STARTUP_MESSAGE_ROOM and HUBOT_STARTUP_MESSAGE must be defined");
     }
