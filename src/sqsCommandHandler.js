207a3602946c4417b5f5e569a1efbb97c232b5aa
Dan Fox <dan.fox@laterooms.com>
2016-08-26 15:41:42 +0100

    fighting teh nodez

--- src/sqsCommandHandler.js
+++ src/sqsCommandHandler.js
@@ -13,9 +13,12 @@ module.exports = function (rbt) {
     robot.catchAll(function (msg) {
         robot.logger.debug("Handling: " + msg.message);
         var rawText = msg.message.rawText;
-        robot.logger.debug("Raw Text: " + msg.message.length);
-        var command = rawText.substr(rawText.indexOf(" "), rawText.length);
+        robot.logger.debug("Raw Text: " + rawText);
+        var command = rawText;
+        if (rawText.indexOf(" ") > 0) {
+            command = rawText.substr(rawText.indexOf(" "), rawText.length);
+        }
         robot.logger.debug("Parsed to: " + command);
-        commandSender.postMessage(command);
+        commandSender.postMessage(command.trim());
     });
 };
